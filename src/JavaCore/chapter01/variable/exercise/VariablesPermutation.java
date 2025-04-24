dfa9b10c0989faf284ed46062615839890dfef6b
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-04-24 14:34:41 +0200

    Update VariablesPermutation.java

--- src/JavaCore/chapter01/variable/exercise/VariablesPermutation.java
+++ src/JavaCore/chapter01/variable/exercise/VariablesPermutation.java
@@ -3,7 +3,20 @@ package javacore.chapter01.variable.exercise;
 public class VariablesPermutation {
     public static void main(String[] args) {
 
-        int a = 5;
+        int a = 15;
         int b = 10;
+
+        //je declare une variable permuteVariableA et j'affecte la valeur de b
+        int permuteVariableA = b;
+
+        //je declare une variable permuteVariableA et j'affecte la valeur de a
+        int permuteVariableB = a;
+
+        //j'affecte la valeur de a à b
+        b = a;
+
+        System.out.println( "La valeur a permuté est égal à " + permuteVariableA );
+
+        System.out.println ( "La valeur b permuté est égal à " + permuteVariableB );
     }
 }
