9c67b4a54d4a03d0b8a9aca9eb1d69ea7aa6ad56
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-04-29 09:32:25 +0200

    Update AgeBaseDiscount.java

--- src/JavaCore/chapter02/condition/exercise/AgeBaseDiscount.java
+++ src/JavaCore/chapter02/condition/exercise/AgeBaseDiscount.java
@@ -10,18 +10,18 @@ public class AgeBaseDiscount {
         // Le forfait Jeune s'applique pour les 25 ans et moins.
         //
         //Le forfait Senior s'applique pour les 65 ans et plus
-        int age = 29;
+        int age = 18;
 
         //Les differentes valeur que je vais tester après avoir écrit le programme:
         //Forfait Basique : 50€
         //
-        //Forfait Jeune : -25% (soit 37,5€)
+        //Forfait Jeune : -25% (soit 37,5€) -> montant - (montant * 25 / 100)
         //
-        //Forfait Senior : -75% (soit 12,5€)
+        //Forfait Senior : -75% (soit 12,5€) -> montant - (montant * 75 / 100)
 
-        double priceWithoutSoldes = 50;
-        double priceForYoungPassenger = priceWithoutSoldes * 0.25;
-        double priceForOlderPassenger = priceWithoutSoldes * 0.50;
+        double priceWithoutSoldes = 100;
+        double priceForYoungPassenger = priceWithoutSoldes - (priceWithoutSoldes * 25 / 100);
+        double priceForOlderPassenger = priceWithoutSoldes - (priceWithoutSoldes * 75/ 100);
 
         if ( age >= 65 ) {
             System.out.println("Vous êtes éligible à la réduction Senior, vous allez payer " + priceForOlderPassenger + " euros");
@@ -32,7 +32,7 @@ public class AgeBaseDiscount {
         }
 
         else {
-            System.out.println("Désolé , vous n'êtes pas éligible à la réduction. Vous allez payer le tarif plein" + priceWithoutSoldes + " euros");
+            System.out.println("Désolé , vous n'êtes pas éligible à la réduction. Vous allez payer le tarif plein " + priceWithoutSoldes + " euros");
         }
     }
 }
