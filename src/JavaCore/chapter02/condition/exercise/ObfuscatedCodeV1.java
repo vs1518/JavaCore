d7615bbb29fe95758a9c77cf602358cfdb7bcaf8
Valeriya <shinvaleriya1808@gmail.com>
2025-05-23 16:40:43 +0200

    ObfuscatedCode

---
+++ src/JavaCore/chapter02/condition/exercise/ObfuscatedCodeV1.java
@@ -0,0 +1,32 @@
+package JavaCore.chapter02.condition.exercise;
+
+public class ObfuscatedCodeV1 {
+
+    public static void main(String[] args) {
+
+        int typeDeVehicule = 2; // Type de véhicule (1 = petite voiture 2 = moyenne voiture, 3 = grande voiture)
+
+        double distance = 100; // Distance
+
+        boolean voitureElectrique = true;
+        double reduction = 0.03;
+
+        double taux = 0;
+
+        if (typeDeVehicule == 1) {
+            taux = distance * 0.10;
+        }
+        if (typeDeVehicule == 2) {
+            taux = distance * 0.20;
+        }
+        if (typeDeVehicule == 3) {
+            taux = distance * 0.35;
+        }
+        if (voitureElectrique) {
+            taux = taux - (distance * reduction);
+        }
+
+        System.out.println("Vous payez pour votre voiture : " + taux + "€");
+
+    }
+}
