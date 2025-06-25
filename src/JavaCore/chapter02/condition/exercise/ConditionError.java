7c993993fa7200b111431d2925b17bf4543df7df
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-06-25 11:55:06 +0200

    New finished exercises

---
+++ src/JavaCore/chapter02/condition/exercise/ConditionError.java
@@ -0,0 +1,33 @@
+package javacore.chapter02.condition.exercise;
+
+public class ConditionError {
+
+    public static void main(String[] args) {
+
+        int age = 77;
+
+        if(age <= 17) {
+
+            System.out.println("Vous êtes mineur et bientôt majeur!");
+
+        }
+        else if(age >= 18 && age <= 59) {
+
+            System.out.println("Vous êtes majeur et adult");
+
+        }
+
+        else if(age >= 60 && age <= 100) {
+
+            System.out.println("Vous n'êtes plus tout jeune.");
+
+        }
+
+        else {
+
+            System.out.println("Vous êtes une exception dans ce monde !");
+
+        }
+
+    }
+}
