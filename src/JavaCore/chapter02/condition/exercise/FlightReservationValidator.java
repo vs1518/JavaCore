72fcd043d72271ad1acf4f92624e82ff293ddf0f
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-07-15 16:45:38 +0200

    Update FlightReservationValidator.java

--- src/JavaCore/chapter02/condition/exercise/FlightReservationValidator.java
+++ src/JavaCore/chapter02/condition/exercise/FlightReservationValidator.java
@@ -8,70 +8,65 @@ public class FlightReservationValidator {
 
         int age = 25;                    //Age d'utilisateur
 
-        int seatAvailable = 40;          //Nombres des places disponible dans l'avion
+        int planeSeatAvailable = 40;          //Nombres des places disponible dans l'avion
 
-        int weightOfBagage = 25;         //Le maximum de kg par bagage
-
-        boolean idPremiumMember = true;  //Est-ce que l'utilisateur est un membre VIP
+        boolean isPremiumMember = true;  //Est-ce que l'utilisateur est un membre VIP
 
         boolean isVipFlight = true;      //Est-ce que le vol est réservé aux VIP?
 
         boolean isFlightLowcost = true;  //Est-ce que le vol est pas cher?
 
-        boolean isBagageInclude = true;  //Est-ce que un bagage est inclus?
+        boolean isBagageIncluded = true;  //Est-ce que un bagage est inclus?
 
 
          //Est-ce que l'utilisateur est connecté ou pas?
         if (!isLoggedIn){
 
             System.out.println("Il faut connecter, s'il vous plaît!");
-            seatAvailable--;
-
+            
         }
 
         //Est- ce que l'utilisateur au moins 18 ans?
         else if (age < 18){
 
             System.out.println("Il faut avoir au moins 18 ans pour réserver le vol!");
-            seatAvailable--;
 
         }
 
         //Est-ce que l'avoin a des places
-        else if (seatAvailable < 0){
+        else if (planeSeatAvailable <= 0){
 
             System.out.println("Il n'y a pas des places disponibles!");
-            seatAvailable--;
 
         }
 
-        //Est-ce que le vol est un VIP , pour un membre Premium et un bagage est inclus?
-        else if (isVipFlight && idPremiumMember && isBagageInclude){
+        //Est-ce que le vol est un VIP , pour un membre Premium?
+        else if (isVipFlight && isPremiumMember){
 
-            System.out.println("Réservation est effectuée avec un succès pour un vol VIP, pour un membre Premium avec un bagage inclus!");
-            seatAvailable--;
+            System.out.println("Réservation est effectuée avec un succès pour un vol VIP, pour un membre Premium!");
+            planeSeatAvailable--;
         }
 
         //Est-ce que le vol non VIP, pour un membre Premium et avec un bagage en plus?
-        else if (!isVipFlight && idPremiumMember && weightOfBagage >= 25){
+        else if (!isVipFlight && isPremiumMember){
 
             System.out.println("Réservation est effectuée avec succès pour un membre Premium avec un bagage en plus!");
-            seatAvailable--;
+            planeSeatAvailable--;
 
         }
 
         //Est-ce que le vol est pas cher et sans bagage inclus
-        else if (isFlightLowcost && !isBagageInclude){
+        else if (isFlightLowcost && !isBagageIncluded){
 
             System.out.println("Réservation est effectuée avec succès pour un vol de lowcost sans bagage!");
-            seatAvailable--;
+            planeSeatAvailable--;
 
         }
 
         //Cas générales
         else if (!isVipFlight) {
             System.out.println("Réservation effectuée avec succès!");
-            seatAvailable--;
+            planeSeatAvailable--;
 
         }
 
