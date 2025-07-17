79a16c4ef2a1d0b327a0a36f0235a6827ffac23d
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-07-17 13:58:08 +0200

    Add files via upload

---
+++ src/JavaCore/chapter03/loop/exercise/WhileLoopExample.java
@@ -0,0 +1,17 @@
+package javacore.chapter03.loop.exercise;
+
+public class WhileLoopExample {
+
+    public static void main(String[] args) {
+
+        int loopCounter = 1;
+
+        while (loopCounter <= 1000) {
+
+            System.out.println("Hello world et -> tour de numero " + loopCounter);
+
+            loopCounter++;
+        }
+    }
+
+}
