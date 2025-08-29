4a798ff2570bf2fecdc0b81ec7c6664f33af4329
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-08-29 23:58:02 +0200

    Update PracticeLoopWhile.java

--- src/javacore/chapter03/loop/practice/PracticeLoopWhile.java
+++ src/javacore/chapter03/loop/practice/PracticeLoopWhile.java
@@ -1,4 +1,39 @@
 package javacore.chapter03.loop.practice;
 
 public class PracticeLoopWhile {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age < 18) {
+            System.out.println("Condition - Age = " + age);
+        }
+
+        while (age < 18) {
+            System.out.println("Boucle - Age = " + age);
+            age++; //age = age + 1 ;
+        }
+
+        System.out.println("On est sortie de la boucle");
+
+
+        int result = 0;
+
+        int currentNumber = 1;
+        int maxNumber = 100;
+
+        while (currentNumber <= maxNumber) {
+
+            System.out.println("Result = " + result + " + " + currentNumber);
+            result += currentNumber; //result = result + currentNumber
+            System.out.println("------> " + result);
+
+            currentNumber++;         // currentNumber = currentNumber + 1;
+
+        }
+
+        System.out.println("Somme des nombres de 1 à 100 : " + result);
+
+    }
 }
