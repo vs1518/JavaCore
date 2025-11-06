0f63900eaddbd2b938bb4b759345e31c7a3f5d27
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-11-06 23:42:50 +0100

    Modif dans le Prime Number exercise

--- src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
+++ src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
@@ -1,4 +1,25 @@
 package javacore.chapter03.loop.exercise;
 
 public class PrimeNumberSearcherV1 {
+
+    public static void main(String[] args) {
+
+        for (int number = 2; number <= 100; number++) {
+
+            boolean isPrime = true;
+
+            for (int divisor = 2; divisor < number; divisor++) {
+
+                if (number % divisor == 0) {
+
+                    isPrime = false;
+                }
+            }
+
+            if (isPrime) {
+
+                System.out.println(number + " est un nombre premier");
+            }
+        }
+    }
 }
