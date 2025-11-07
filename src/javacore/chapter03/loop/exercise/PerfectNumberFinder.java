2c0ff5cc1bfb880b26bd1458ed858c51fede3570
Valeriya <121875455+vs1518@users.noreply.github.com>
2025-11-07 16:43:39 +0100

    PerfectNumberFinder
    Add a program to find and print perfect numbers between 1 and 1000.

--- src/javacore/chapter03/loop/exercise/PerfectNumberFinder.java
+++ src/javacore/chapter03/loop/exercise/PerfectNumberFinder.java
@@ -1,4 +1,28 @@
 package javacore.chapter03.loop.exercise;
 
 public class PerfectNumberFinder {
+
+    public static void main(String[] args) {
+
+        int start = 1;
+        int end = 1000;
+
+        for (int number = start; number <= end; number++) {
+
+            int sum = 0;
+
+            for (int divisor = 1; divisor < number; divisor++) {
+
+                if (number % divisor == 0) {
+
+                    sum = sum + divisor;
+                }
+            }
+
+            if (sum == number) {
+                System.out.println(number + "est un nombre parfait");
+            }
+        }
+
+    }
 }
